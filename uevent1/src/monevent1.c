97a94df00d0a624b6c5a09726ba026d3b468bf1d
aleksander <aleksander.mozetic@gmail.com>
2019-04-05 18:37:28 +0200

    Wireless interface scanning

--- uevent1/src/monevent1.c
+++ uevent1/src/monevent1.c
@@ -561,6 +561,40 @@ void print_ampdu_spacing(__u8 spacing)
 	       print_ampdu_space(spacing), spacing);
 }
 
+static void print_mcs_index(const __u8 *mcs)
+{
+	int mcs_bit, prev_bit = -2, prev_cont = 0;
+
+	for (mcs_bit = 0; mcs_bit <= 76; mcs_bit++) {
+		unsigned int mcs_octet = mcs_bit/8;
+		unsigned int MCS_RATE_BIT = 1 << mcs_bit % 8;
+		bool mcs_rate_idx_set;
+
+		mcs_rate_idx_set = !!(mcs[mcs_octet] & MCS_RATE_BIT);
+
+		if (!mcs_rate_idx_set)
+			continue;
+
+		if (prev_bit != mcs_bit - 1) {
+			if (prev_bit != -2)
+				printf("%d, ", prev_bit);
+			else
+				printf(" ");
+			printf("%d", mcs_bit);
+			prev_cont = 0;
+		} else if (!prev_cont) {
+			printf("-");
+			prev_cont = 1;
+		}
+
+		prev_bit = mcs_bit;
+	}
+
+	if (prev_cont)
+		printf("%d", prev_bit);
+	printf("\n");
+}
+
 void print_ht_mcs(const __u8 *mcs)
 {
 	/* As defined in 7.3.2.57.4 Supported MCS Set field */
